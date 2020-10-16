d8c2bbbac95b80827678969de68153f7b62c804d
Daniel Hagen <daniel.b.hagen@gmail.com>
2020-10-16 16:03:37 -0500

    Removing theme messing with lint
    Signed-off-by: Daniel B. Hagen <daniel.b.hagen@gmail.com>

---
+++ .storybook/addons.js
@@ -0,0 +1,5 @@
+import '@storybook/addon-viewport'
+import '@storybook/addon-a11y'
+import '@storybook/addon-controls'
+import 'storybook-dark-mode/register'
+import 'storybook-addon-theme-playground/dist/register'
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,16 @@
 import { addDecorator } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
 import '../styles/tailwind.css'
+import { withThemePlayground } from 'storybook-addon-theme-playground';
+
+import { theme } from '../tailwind.config';
+
+export const decorators = [
+  withThemePlayground({
+    theme,
+    provider: ThemeProvider,
+  }),
+];
 
 addDecorator(withA11y);
 
